fec9537ce3c227de39cc17e419941dde59f8e5bd
rudyab <rubylinderbyrne@gmail.com>
2017-10-16 17:30:59 -0600

    upload plots to eor_cloud (replace devel location)

--- aws_costs_cron.sh
+++ aws_costs_cron.sh
@@ -2,6 +2,6 @@
 
 /home/rlbyrne/anaconda2/bin/python /home/rlbyrne/rlb_aws/cost_plotter.py
 plotname=$(ls /home/rlbyrne/rlb_aws/aws_costs_*.png)
-/home/rlbyrne/slackcat --channel rubyb ${plotname}
+/home/rlbyrne/slackcat --channel eor_cloud ${plotname}
 rm /home/rlbyrne/rlb_aws/cost_report-1.csv
 rm ${plotname}
