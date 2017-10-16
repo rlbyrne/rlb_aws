4693a9c154e1bfb8c2508753fcc4c3dbf2336c16
rudyab <rubylinderbyrne@gmail.com>
2017-10-16 16:37:53 -0600

    use anaconda

--- aws_costs_cron.sh
+++ aws_costs_cron.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/home/rlbyrne/rlb_aws/cost_plotter.py
+/home/rlbyrne/anaconda2/bin/python /home/rlbyrne/rlb_aws/cost_plotter.py
 plotname=$(ls /home/rlbyrne/rlb_aws/aws_costs_*.png)
 /home/rlbyrne/slackcat --channel rubyb /home/rlbyrne/rlb_aws/${plotname}
 rm /home/rlbyrne/rlb_aws/cost_report-1.csv
