247b80796a5fee539930a0baf745d0d543b3283f
rudyab <rubylinderbyrne@gmail.com>
2017-10-16 17:21:02 -0600

    bug fix

--- aws_costs_cron.sh
+++ aws_costs_cron.sh
@@ -2,6 +2,6 @@
 
 /home/rlbyrne/anaconda2/bin/python /home/rlbyrne/rlb_aws/cost_plotter.py
 plotname=$(ls /home/rlbyrne/rlb_aws/aws_costs_*.png)
-/home/rlbyrne/slackcat --channel rubyb /home/rlbyrne/rlb_aws/${plotname}
+/home/rlbyrne/slackcat --channel rubyb ${plotname}
 rm /home/rlbyrne/rlb_aws/cost_report-1.csv
-rm /home/rlbyrne/rlb_aws/${plotname}
+rm ${plotname}
